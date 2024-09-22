d650fa40e0a0ef072673002f9b065bfb23e590f7
ezzbakry <ezzeldenamer@gmail.com>
2024-09-22 23:20:35 +0300

    login backend

--- src/pages/Form/Login.jsx
+++ src/pages/Form/Login.jsx
@@ -2,42 +2,56 @@ import Form from 'react-bootstrap/Form';
 import mystyle from './Login.module.css'
 import Button from 'react-bootstrap/Button';
 import { useForm } from "react-hook-form"
-
+import React, { useState } from 'react';
+import axios from 'axios';
 
 
 export default function LoginLib() {
-    const { register, handleSubmit, formState: { errors } } = useForm({mode:"onChange"})
+    const { register, handleSubmit, formState: { errors } } = useForm({ mode: "onChange" })
+    // const [name, setname] = useState("")
+    const [email, setemail] = useState("")
+    const [password, setpassword] = useState("")
+    const [message, setmessage] = useState("")
 
 
-    const login = (user) => {
-        console.log(user)
+    const login = async (user) => {
+        setmessage("")
+        try {
+            const res = await axios.post("http://localhost:3000/users/login", { email, password })
+            console.log(res.data.message)
+            const { token } = res.data
+            localStorage.setItem('token', token)
+            setmessage("logged in successfully")
+        } catch (e) {
+            setmessage(e.res?.data.message || e.res.message)
+        }
     }
 
     return <>
+        <p>{message}</p>
         <div className={mystyle.test}>
             <Form onSubmit={handleSubmit(login)}>
-                {/* <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" >
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" className={`form-control ${(errors.name)?"border-danger shadow-none":""}`} placeholder="Enter the name" {...register("name", { required: true })} />
-                </Form.Group>
-                {errors.name && <p className='text-danger'>name is required</p>} */}
 
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control  className={`form-control ${(errors.email)?'border-danger shadow-none':""}`} type="email" placeholder="name@example.com" {...register('email', { required: true, pattern: /^[a-zA-Z]{3,8}[0-9]{1,3}(@)(gmail|yahoo)(.com)$/ })} />
+                    <Form.Control className={`form-control ${(errors.email) ? 'border-danger shadow-none' : ""}`} type="email" placeholder="name@example.com" {...register('email', {
+                        required: true, onChange: (e) => {
+                            setemail(e.target.value)
+                        }, pattern: /^[a-zA-Z]{3,8}[0-9]{1,3}(@)(gmail|yahoo)(.com)$/
+                    })} />
                 </Form.Group>
                 {errors.email && <p className='text-danger'>invalid or required email</p>}
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control className={`form-control ${errors.password?"border-danger shadow-none":""}`} type="password" placeholder="Enter the password" {...register("password", { required: true,pattern: /[._!?@=]/ })} />
+                    <Form.Control className={`form-control ${errors.password ? "border-danger shadow-none" : ""}`} type="password" placeholder="Enter the password" {...register("password", {
+                        required: true, onChange: (e) => {
+                            setpassword(e.target.value)
+                        }, pattern: /^[a-zA-Z0-9]{3,15}[@.?-_=]/
+                    })} />
                 </Form.Group>
                 {errors.password && <p className='text-danger'>invalid or required password</p>}
-                {/* <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Phone</Form.Label>
-                    <Form.Control className={`form-control ${errors.phone?"border-danger shadow-none":""}`} type="number" placeholder="Enter the phone" {...register("phone", { required: true, pattern: /[0-9]{11}/ })} />
-                </Form.Group>
-                {errors.phone && <p className='text-danger'>invalid or required phone</p>} */}
-                <Button as="input" type="submit" value="Submit" />{' '}
+
+                <Button as="input" type="submit" value="Login" />{' '}
 
             </Form>
 
--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -13,44 +13,52 @@ export default function Register() {
     const [password, setpassword] = useState("")
     const [message, setmessage] = useState("")
     // const load = useSelector((state) => state.load.load)
-    const { register, handleSubmit, watch, formState: { errors } } = useForm({ mode: "onChange" })
-    // const onSubmit = (data) => console.log(data)
-
-
+    const { register, handleSubmit, formState: { errors } } = useForm({ mode: "onChange" })
 
     const Register = async (user) => {
-        const Name = user.name
-        const Email = user.email
-        const Password = user.password
-        setname(Name)
-        setemail(Email)
-        setpassword(Password)
-        try {
-            const res = await axios.post('http://localhost:3000/users/', { Name, Email, Password })
-            setmessage("successfully")
-        } catch (err) {
-            setmessage("failed")
-        }
-        //err.res?.res.message||
+        try{
+            const res = await axios.post("http://localhost:3000/users", { name, email, password })
+            setmessage(res.data.message)
+            console.log(res.data)
+        }catch (err){
+            setmessage(err.res?.data.message||err.res.message)}
     }
     return <>
         <p> {message}</p>
+
+        {/* <p> {name}</p> */}
         <div className={mystyle.test}>
             <Form onSubmit={handleSubmit(Register)}>
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1" >
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" className={`form-control ${(errors.name) ? "border-danger shadow-none" : ""}`} placeholder="Enter the name" {...register("name", { required: true })} />
+                    <Form.Control type="text" className={`form-control ${(errors.name) ? "border-danger shadow-none" : ""}`} placeholder="Enter the name" {...register("name", {
+                        required: true, onChange: (e) => {
+                            setname(e.target.value)
+                        }
+                    })} />
                 </Form.Group>
                 {errors.name && <p className='text-danger'>name is required</p>}
 
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>Email address</Form.Label>
-                    <Form.Control className={`form-control ${(errors.email) ? 'border-danger shadow-none' : ""}`} type="email" placeholder="name@example.com" {...register('email', { required: true, pattern: /^[a-zA-Z]{3,8}[0-9]{1,3}(@)(gmail|yahoo)(.com)$/ })} />
+                    <Form.Control onChange={(e) => {
+                        setemail(e.target.value)
+                    }} className={`form-control ${(errors.email) ? 'border-danger shadow-none' : ""}`} type="email" placeholder="name@example.com" {...register('email', {
+                        required: true, onChange: (e) => {
+                            setemail(e.target.value)
+                        }, pattern: /^[a-zA-Z0-9]{3,9}@(gmail|yahoo)(.com)$/
+                    })} />
                 </Form.Group>
                 {errors.email && <p className='text-danger'>invalid or required email</p>}
                 <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control className={`form-control ${errors.password ? "border-danger shadow-none" : ""}`} type="password" placeholder="Enter the password" {...register("password", { required: true, pattern: /[._!?@=]/ })} />
+                    <Form.Control onChange={(e) => {
+                        setpassword(e.target.value)
+                    }} className={`form-control ${errors.password ? "border-danger shadow-none" : ""}`} type="password" placeholder="Enter the password" {...register("password", {
+                        required: true, onChange: (e) => {
+                            setpassword(e.target.value)
+                        }, pattern: /^[a-zA-Z0-9]{3,15}[@.?-_=]/
+                    })} />
                 </Form.Group>
                 {errors.password && <p className='text-danger'>invalid or required password</p>}
                 {/* <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -58,7 +66,7 @@ export default function Register() {
                     <Form.Control className={`form-control ${errors.phone?"border-danger shadow-none":""}`} type="number" placeholder="Enter the phone" {...register("phone", { required: true, pattern: /[0-9]{11}/ })} />
                 </Form.Group>
                 {errors.phone && <p className='text-danger'>invalid or required phone</p>} */}
-                <Button onSubmit={Register()} as="input" type="submit" value="Register" />{' '}
+                <Button as="input" type="submit" value="Register" />{' '}
             </Form>
 
 
