edea4a12b443ee928f1021d3ef4a013bf8c23a91
Ezz Eldeen Amer Ali <91624469+ezzbakry@users.noreply.github.com>
2024-10-04 19:45:28 +0300

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,6 @@ import { changename } from '../../Store/slices/name'
 import { changeLanguage } from '../../Store/slices/language'
 import { increaseCounter } from '../../Store/slices/counter'
 import { reduceCounter } from '../../Store/slices/counter'
-import { changebg } from "../../Store/slices/background"
-import { changecol } from "../../Store/slices/font"
 import { namecontext } from "../../context/name"
 import { useContext } from "react"
 import { useReducer } from "react"
@@ -27,8 +25,6 @@ export default function Home() {
     const lang = useSelector((state) => state.language.language)
     const name = useSelector((state) => state.name.name)
     // const count = useSelector((state) => state.counter.counter)
-    const bg = useSelector((state) => state.bg.bg)
-    const f = useSelector((state) => state.fontcol.fontcol)
     const { Name, setname } = useContext(namecontext)
     const [state, disp] = useReducer(reducer, initialcounter)
 
@@ -40,17 +36,7 @@ export default function Home() {
     const togglelang = () => {
         dispatch(changeLanguage((lang == "en") ? "ar" : "en"))
     }
-    // const increasecounter = () => {
-    //     dispatch(increaseCounter())
-    // }
-    // const reducecounter = () => {
-    //     dispatch(reduceCounter())
-    // }
-    const change = () => {
-        dispatch(changebg((bg == "white") ? "black" : "white"))
-        dispatch(changecol((f == "black") ? 'white' : "black"))
-    }
-
+    
     return <>
         <div style={{ display: "flex" }}>
             <div style={{ margin: "40px" }}>
@@ -117,17 +103,6 @@ export default function Home() {
 
         </div>
         <div style={{ display: "flex" }}>
-            <div style={{ marginLeft: "40px" }}>
-
-                <button style={{ marginTop: "40px" }}
-                    className="btn btn-success"
-                    onClick={() => {
-                        change();
-                    }}
-                >
-                    change bg and font color
-                </button>
-            </div>
             <div style={{ marginLeft: "40px" }}>
                 <p>{Name}</p>
                 <button
